3ad24cdd47c63f374509e7e86e319ff1bbea1b51
bagatka <alex.bagatka@gmail.com>
2020-09-08 20:51:20 +0300

    Added SQL Server Db Project

---
+++ CinemaDb/dbo/Tables/cinemas.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[cinemas] (
+    [id]          INT             NOT NULL,
+    [name]        NVARCHAR (50)   NOT NULL,
+    [description] NVARCHAR (1000) NOT NULL,
+    [city]        NVARCHAR (50)   NOT NULL,
+    [image_url]   VARCHAR (2048)  NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
