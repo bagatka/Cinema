d163054a99479f64b20c431827d1105700666f6c
bagatka <alex.bagatka@gmail.com>
2020-09-09 13:58:59 +0300

    Database added and fixed naming. Added EnittyFrameworkCore and models. Added SQL project. Connected SQL and EntityFrameWorkCore. Fixed namespace.

---
+++ iTechArt.CinemaWebApp.API/Data/CinemaDbContext.cs
@@ -0,0 +1,21 @@
+﻿using iTechArt.CinemaWebApp.API.Model;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace iTechArt.CinemaWebApp.API.Data
+{
+    public class CinemaDbContext : DbContext
+    {
+        public DbSet<Film> Films { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=Cinema;Trusted_Connection=True");
+        }
+
+        public CinemaDbContext(DbContextOptions<CinemaDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ iTechArt.CinemaWebApp.API/Model/Film.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace iTechArt.CinemaWebApp.API.Model
+{
+    public class Film
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("title")]
+        public string Title { get; set; }
+        [Column("description")]
+        public string Description { get; set; }
+        [Column("poster_url")]
+        public string PosterUrl { get; set; }
+        [Column("banner_url")]
+        public string BannerUrl { get; set; }
+    }
+}
