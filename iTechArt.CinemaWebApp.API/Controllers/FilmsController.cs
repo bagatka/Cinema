fb5d8e57678b6a232f3fc87691c16b55315760e0
bagatka <alex.bagatka@gmail.com>
2020-09-09 18:49:03 +0300

    Film controller upgrade. Added UpdateFilm and DeleteFilm methods. Fixed CreateFilm and GetFilmById methods.

--- iTechArt.CinemaWebApp.API/Controllers/FilmsController.cs
+++ iTechArt.CinemaWebApp.API/Controllers/FilmsController.cs
@@ -2,6 +2,7 @@ using iTechArt.CinemaWebApp.API.Data;
 using iTechArt.CinemaWebApp.API.Model;
 
 using Microsoft.AspNetCore.Mvc;
+
 using Microsoft.EntityFrameworkCore;
 
 using System.Collections.Generic;
@@ -21,28 +22,77 @@ namespace iTechArt.CinemaWebApp.API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Film>>> GetFilmByName([FromQuery] string name)
+        public async Task<ActionResult<IEnumerable<Film>>> GetFilmByName([FromQuery] string title)
         {
             var films = _context.Films.AsQueryable();
 
-            if(!string.IsNullOrEmpty(name))
+            if(!string.IsNullOrEmpty(title))
             {
-                films = films.Where(n => n.Title.Contains(name));
+                films = films.Where(n => n.Title.Contains(title));
             }
 
-            return await films.OrderBy(num => num.Id).ToListAsync();
+            return await films.OrderBy(film => film.Id).ToListAsync();
         }
+
         [HttpPost]
-        public async Task<ActionResult<Film>> CreateHero(Film film)
+        public async Task<ActionResult<Film>> CreateFilm(Film film)
         {
             Film newFilm = new Film()
             {
-                Title = film.Title
+                Title = film.Title,
+                Description = film.Description,
+                PosterUrl = (!string.IsNullOrEmpty(film.PosterUrl)) ? film.PosterUrl : "",
+                BannerUrl = (!string.IsNullOrEmpty(film.BannerUrl)) ? film.BannerUrl : ""
             };
             await _context.Films.AddAsync(newFilm);
             await _context.SaveChangesAsync();
 
-            return Ok();
+            return StatusCode(201);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateFilm(int id, [FromBody] Film film)
+        {
+            if (id != film.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(film).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FilmExists(id))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Film>> DeleteFilm(int id)
+        {
+            var resultFilm = await _context.Films.FindAsync(id);
+            if (resultFilm == null)
+            {
+                return NotFound();
+            }
+            _context.Films.Remove(resultFilm);
+            await _context.SaveChangesAsync();
+
+            return resultFilm;
+        }
+
+        private bool FilmExists(int id)
+        {
+            return _context.Films.Any(film => film.Id == id);
         }
     }
 }
