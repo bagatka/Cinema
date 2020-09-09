f980f554e65494811c3d73aad207116b5dd32a1e
bagatka <alex.bagatka@gmail.com>
2020-09-09 17:41:42 +0300

    Fixed cinema and film models

--- iTechArt.CinemaWebApp.API/Model/Cinema.cs
+++ iTechArt.CinemaWebApp.API/Model/Cinema.cs
@@ -7,15 +7,10 @@ namespace iTechArt.CinemaWebApp.API.Model
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("id")]
         public int Id { get; set; }
-        [Column("name")]
         public string Name { get; set; }
-        [Column("description")]
         public string Description { get; set; }
-        [Column("city")]
         public string City { get; set; }
-        [Column("image_url")]
         public string ImageUrl { get; set; }
     }
 }
--- iTechArt.CinemaWebApp.API/Model/Film.cs
+++ iTechArt.CinemaWebApp.API/Model/Film.cs
@@ -7,15 +7,10 @@ namespace iTechArt.CinemaWebApp.API.Model
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("id")]
         public int Id { get; set; }
-        [Column("title")]
         public string Title { get; set; }
-        [Column("description")]
         public string Description { get; set; }
-        [Column("poster_url")]
         public string PosterUrl { get; set; }
-        [Column("banner_url")]
         public string BannerUrl { get; set; }
     }
 }
