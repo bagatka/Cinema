64446d927f9f695c834ecac7ea729d5eb5ab0294
bagatka <alex.bagatka@gmail.com>
2020-09-15 19:06:00 +0300

    Added flex layout library to the ClientApp

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 import {HomeScreenComponent} from './home-screen/home-screen.component';
+import {SearchScreenComponent} from './search-screen/search-screen.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeScreenComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'cinemas', component: HomeScreenComponent}
+  {path: 'cinemas', component: HomeScreenComponent},
+  {path: 'films', component: SearchScreenComponent}
 ];
 
 @NgModule({
