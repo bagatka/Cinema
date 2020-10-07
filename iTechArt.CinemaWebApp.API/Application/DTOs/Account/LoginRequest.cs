f2acc3a08aa38bad67b774bcd7217b91995cbe80
bagatka <alex.bagatka@gmail.com>
2020-10-07 21:42:37 +0300

    Added validation to login request

--- iTechArt.CinemaWebApp.API/Application/DTOs/Account/LoginRequest.cs
+++ iTechArt.CinemaWebApp.API/Application/DTOs/Account/LoginRequest.cs
@@ -1,8 +1,14 @@
-﻿namespace iTechArt.CinemaWebApp.API.Application.DTOs.Account
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace iTechArt.CinemaWebApp.API.Application.DTOs.Account
 {
     public class LoginRequest
     {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password length can't be less than 6 symbols.")]
         public string Password { get; set; }
     }
 }
